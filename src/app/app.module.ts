91881541429b2cf003a97fbbbdf8fb2b8b02a0b3
wei.liu <Wei.Liu@gfk.com>
2021-03-05 12:38:03 +0100

    configure angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {MatToolbarModule} from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+    imports: [
+        BrowserModule,
+        MatToolbarModule
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
