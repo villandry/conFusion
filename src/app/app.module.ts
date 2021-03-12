39d3d140c71e1be32bffde4b40909caf0207b212
wei.liu <Wei.Liu@gfk.com>
2021-03-12 09:42:18 +0100

    leader service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import {AppRoutingModule} from './app-routing/app-routing.module';
+import {LeaderService} from './services/leader.service';
+import {PromotionService} from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -37,7 +39,7 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
         FlexLayoutModule,
         AppRoutingModule
     ],
-  providers: [DishService],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Dish} from '../shared/dish';
 import {DISHES} from '../shared/dishes';
 import {Promotion} from '../shared/promotion';
 import {DishService} from '../services/dish.service';
 import {PromotionService} from '../services/promotion.service';
+import {Leader} from '../shared/leader';
+import {LeaderService} from '../services/leader.service';
 
 @Component({
   selector: 'app-home',
@@ -14,12 +16,16 @@ export class HomeComponent implements OnInit {
 
   dish: Dish;
   promotion: Promotion;
+  featuredLeader: Leader;
 
   constructor(private dishservice: DishService,
-              private promotionservice: PromotionService) { }
+              private promotionservice: PromotionService,
+              private leaderService: LeaderService) {
+  }
 
   ngOnInit() {
     this.dish = this.dishservice.getFeaturedDish();
     this.promotion = this.promotionservice.getFeaturedPromotion();
+    this.featuredLeader = this.leaderService.getFeaturedLeader();
   }
 }
