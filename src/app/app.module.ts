d718f36a4358e25facee4d7b596b90ed06eeecb5
wei.liu <Wei.Liu@gfk.com>
2021-03-16 09:11:17 +0100

    dialogues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {MatButtonModule, MatCardModule, MatGridListModule, MatListModule, MatToolbarModule} from '@angular/material';
-import { MenuComponent } from './menu/menu.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MenuComponent} from './menu/menu.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import {DishdetailComponent} from './dishdetail/dishdetail.component';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {DishService} from './services/dish.service';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {AboutComponent} from './about/about.component';
+import {HomeComponent} from './home/home.component';
+import {ContactComponent} from './contact/contact.component';
 import {AppRoutingModule} from './app-routing/app-routing.module';
 import {LeaderService} from './services/leader.service';
 import {PromotionService} from './services/promotion.service';
+import {LoginComponent} from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,20 +28,26 @@ import {PromotionService} from './services/promotion.service';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        MatToolbarModule,
-        MatListModule,
-        MatGridListModule,
-        MatCardModule,
-        MatButtonModule,
-        FlexLayoutModule,
-        AppRoutingModule
-    ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule,
+    FlexLayoutModule,
+    AppRoutingModule
+  ],
+  entryComponents: [
+    LoginComponent
+  ],
   providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
